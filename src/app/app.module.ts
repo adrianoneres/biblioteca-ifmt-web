faf5a455ec63fa4c817793e3b0ac9c2dc101b285
Adriano Souza <ah.driano@gmail.com>
2023-11-01 19:53:13 -0400

    feat: adiciona router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,22 @@ import { ButtonComponent } from './button/button.component';
 import { InputTextComponent } from './input-text/input-text.component';
 import { FormsModule } from '@angular/forms';
 import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/sign-in', pathMatch: 'full' },
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'sign-up', component: SignUpComponent }
+];
 @NgModule({
   declarations: [
     AppComponent,
-    SignInComponent
+    SignInComponent,
+    SignUpComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     TitleComponent,
     ButtonComponent,
