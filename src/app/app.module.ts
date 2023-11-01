ecb8de8147e5f5558a2cf5453ea844a83eea20ef
Adriano Souza <ah.driano@gmail.com>
2023-10-31 20:09:19 -0400

    chore: estrutura inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TitleComponent } from './title/title.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TitleComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/title/title.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-title',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './title.component.html',
+  styleUrls: ['./title.component.scss']
+})
+export class TitleComponent {
+  @Input() valor!: string;
+}
