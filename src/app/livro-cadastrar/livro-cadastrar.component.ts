f0eef151553802b30082e9d8a001ade5942e293a
Adriano Souza <ah.driano@gmail.com>
2023-11-08 19:49:29 -0400

    feat: adiciona implementação da classe de cadastro de livros

--- src/app/livro-cadastrar/livro-cadastrar.component.ts
+++ src/app/livro-cadastrar/livro-cadastrar.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Livro } from '../livro';
+import { BackendService } from '../backend.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-livro-cadastrar',
@@ -9,7 +11,15 @@ import { Livro } from '../livro';
 export class LivroCadastrarComponent {
   livro: Livro;
 
-  constructor() {
+  constructor(private backendService: BackendService, private router: Router) {
     this.livro = { titulo: '', autor: '' };
+  }
+
+  salvar() {
+    // 1. Chamar o serviço para salvar o livro
+    // 2. Ao salvar o livro, redirecionar para a tela de listagem
+    this.backendService
+      .criarLivro(this.livro)
+      .then(resposta => this.router.navigate(['/livros']));
   }
 }
